4a6473a44abfc95cfaecae9f8304f98d0a19750f
PopovVK <vladsmartvladsmart@gmail.com>
2024-03-06 15:58:07 +0200

    Start catalog

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  background: conic-gradient(
+    from 100deg at 50% 50%,
+    rgba(5, 66, 44, 1),
+    rgba(100, 138, 124, 1),
+    rgba(5, 66, 44, 1)
+  );
+  height: 100%;
+  width: 100%;
+  padding: 3rem 4.5rem;
+  margin-top: 7.85rem;
+`;
+
+
 
 export default function Catalog() {
   return (
-    <section>
-      <div>this is a catalog</div>
-    </section>
+    <Wrapper>
+      <div>
+        <section>
+          <article>
+            <img src="../components/images/img1.png" alt="img1" />
+            <p>FLOWER</p>
+            <h4>2 Oz Deal Watermelon Zkittles + Purple Gushers</h4>
+          </article>
+          <article>
+          <img src="../components/images/img2.png" alt="img2" />
+            <p>FLOWER</p>
+            <h4>2 Oz Deal Ahi Tuna + Master Tuna</h4>
+          </article>
+          <article>
+          <img src="../components/images/img3.png" alt="img3" />
+            <p>CONCENTRATES</p>
+            <h4>Mix And Match Shatter/Budder 28g (4 X 7G)</h4>
+          </article>
+        </section>
+      </div>
+    </Wrapper>
   );
 }
