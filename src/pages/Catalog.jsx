02580ffe92cf1478e2f539e0fe374a32dbe32529
PopovVK <vladsmartvladsmart@gmail.com>
2024-03-11 21:52:19 +0200

    Final-TS5

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,43 +1,151 @@
 import React from "react";
 import styled from "styled-components";
+import img1 from "../components/images/img1.jpg";
+import img2 from "../components/images/img2.jpg";
+import img3 from "../components/images/img3.jpg";
+import left from "../components/images/left.png";
+import right from "../components/images/right.png";
 
-const Wrapper = styled.section`
-  background: conic-gradient(
-    from 100deg at 50% 50%,
-    rgba(5, 66, 44, 1),
-    rgba(100, 138, 124, 1),
-    rgba(5, 66, 44, 1)
-  );
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 75px;
+  padding: 100px 0px 100px 0px;
   height: 100%;
   width: 100%;
-  padding: 3rem 4.5rem;
-  margin-top: 7.85rem;
+  background-color:#F2F6F4;
+  
+  
 `;
 
+const CartWrapper = styled.div`
+  display: flex;
+ flex-direction: row;
+ justify-content: space-around;
+
+  `;
+
+const ArrowLeft = styled.button`
+  position: absolute;
+  left: 110px;
+  bottom: 240px;
+  border-radius: 100px;
+  width: 40px;
+  height: 40px;
+  background-color: #FFFFFF;
+  border: none;
+  cursor: pointer;
+  
+  `;
+const ArrowRight = styled.button`
+  position: absolute;
+  right: 110px;
+  bottom: 240px;
+  border-radius: 100px;
+  width: 40px;
+  height: 40px;
+  background-color: #FFFFFF;
+  border: none;
+  cursor: pointer;
+  `;
+
+const ImgLeft = styled.img`
+  width: 20px;
+  height: 20px;
+  
+  `;
+const ImgRight = styled.img`
+  width: 20px;
+  height: 20px;
+  `;
+const CartContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  width: 280px;
+  height: 400px;
+  
+  
+  `;
+const CartImg = styled.img`
+  width: 280px;
+  height: 240px;
+  
+  `;
+const TextCategory = styled.p`
+  margin-top: 15px;
+  text-align: center;
+  color: #9D9EA2; 
+  font-weight: 300;
+  font-size: 14px; 
+  font-family: Lexend;
+  line-height: 21px;
+  letter-spacing: 0px;
+
+
+  `;
+const TextCart = styled.h3`
+  text-align: center;
+  color: #1A1E26; 
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 27px;  
+  font-family: Lexend;
+  letter-spacing: 0px;
+
+  `;
+
+
+  const Button = styled.button`
+
+    padding: 0px, 24px, 0px, 24px;
+    background-color: #17AF26;
+    border: none;
+    cursor: pointer;
+    width: 128px;
+    height: 40px;
+    border-radius: 100px;
+    color: #FFFFFF;
+    font-family: Lexend;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 21px;
+    letter-spacing: 0px;
+
+    
+  `;
 
 
 export default function Catalog() {
   return (
     <Wrapper>
-      <div>
-        <section>
-          <article>
-            <img src="../components/images/img1.png" alt="img1" />
-            <p>FLOWER</p>
-            <h4>2 Oz Deal Watermelon Zkittles + Purple Gushers</h4>
-          </article>
-          <article>
-          <img src="../components/images/img2.png" alt="img2" />
-            <p>FLOWER</p>
-            <h4>2 Oz Deal Ahi Tuna + Master Tuna</h4>
-          </article>
-          <article>
-          <img src="../components/images/img3.png" alt="img3" />
-            <p>CONCENTRATES</p>
-            <h4>Mix And Match Shatter/Budder 28g (4 X 7G)</h4>
-          </article>
-        </section>
-      </div>
+        <CartWrapper>
+          <CartContainer>
+            <ArrowLeft>
+              <ImgLeft src={left} alt="left" />
+            </ArrowLeft> 
+            <CartImg src={img1} alt="img1" />
+            <TextCategory>FLOWER</TextCategory>
+            <TextCart>2 Oz Deal Watermelon Zkittles + Purple Gushers</TextCart>
+            <Button>Add to Cart</Button>
+          </CartContainer>
+          <CartContainer>
+            <CartImg src={img2} alt="img2" />
+            <TextCategory>FLOWER</TextCategory>
+            <TextCart>2 Oz Deal Ahi Tuna + Master Tuna</TextCart>
+            <Button>Add to Cart</Button>
+          </CartContainer>
+          <CartContainer>
+            <ArrowRight>
+              <ImgRight src={right} alt="right" />
+            </ArrowRight> 
+            <CartImg src={img3} alt="img3" />
+            <TextCategory>CONCENTRATES</TextCategory>
+            <TextCart>Mix And Match Shatter/Budder 28g (4 X 7G)</TextCart>
+            <Button>Add to Cart</Button>
+          </CartContainer>
+        </CartWrapper>
     </Wrapper>
   );
 }
