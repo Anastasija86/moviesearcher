d80a436196ef24bb1857d1075dcc48b8648222db
PopovVK <vladsmartvladsmart@gmail.com>
2024-04-12 11:28:05 +0300

    add API

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,151 +1,111 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import img1 from "../components/images/img1.jpg";
-import img2 from "../components/images/img2.jpg";
-import img3 from "../components/images/img3.jpg";
-import left from "../components/images/left.png";
-import right from "../components/images/right.png";
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 75px;
-  padding: 100px 0px 100px 0px;
+  align-items: center;
+  margin-top: 50px;
+  padding: 100px 20px;
   height: 100%;
   width: 100%;
-  background-color:#F2F6F4;
-  
-  
+  background-color: #f2f6f4;
 `;
 
 const CartWrapper = styled.div`
   display: flex;
- flex-direction: row;
- justify-content: space-around;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 10px;
+  max-width: 1200px;
+  width: 100%;
+`;
 
-  `;
-
-const ArrowLeft = styled.button`
-  position: absolute;
-  left: 110px;
-  bottom: 240px;
-  border-radius: 100px;
-  width: 40px;
-  height: 40px;
-  background-color: #FFFFFF;
-  border: none;
-  cursor: pointer;
-  
-  `;
-const ArrowRight = styled.button`
-  position: absolute;
-  right: 110px;
-  bottom: 240px;
-  border-radius: 100px;
-  width: 40px;
-  height: 40px;
-  background-color: #FFFFFF;
-  border: none;
-  cursor: pointer;
-  `;
-
-const ImgLeft = styled.img`
-  width: 20px;
-  height: 20px;
-  
-  `;
-const ImgRight = styled.img`
-  width: 20px;
-  height: 20px;
-  `;
 const CartContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
   align-items: center;
+  margin: 0 10px 20px;
   width: 280px;
-  height: 400px;
-  
-  
-  `;
+  background-color: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
 const CartImg = styled.img`
   width: 280px;
-  height: 240px;
-  
-  `;
-const TextCategory = styled.p`
-  margin-top: 15px;
+  height: 400px;
+  border-radius: 10px 10px 0 0;
+`;
+
+const TextName = styled.p`
   text-align: center;
-  color: #9D9EA2; 
-  font-weight: 300;
-  font-size: 14px; 
+  margin-top: 15px;
+  margin-bottom: 5px;
+  color: #1a1e26;
+  font-weight: 500;
+  font-size: 18px;
   font-family: Lexend;
   line-height: 21px;
   letter-spacing: 0px;
+`;
 
-
-  `;
-const TextCart = styled.h3`
+const TextData = styled.p`
   text-align: center;
-  color: #1A1E26; 
-  font-weight: 400;
-  font-size: 18px;
-  line-height: 27px;  
+  margin-bottom: 10px;
+  color: #9d9ea2;
+  font-size: 14px;
+  font-family: Lexend;
+  line-height: 21px;
+  letter-spacing: 0px;
+`;
+
+const Rating = styled.p`
+  text-align: center;
+  margin-bottom: 20px;
+  color: #1a1e26;
+  font-weight: 500;
+  font-size: 12px;
   font-family: Lexend;
   letter-spacing: 0px;
-
-  `;
-
-
-  const Button = styled.button`
-
-    padding: 0px, 24px, 0px, 24px;
-    background-color: #17AF26;
-    border: none;
-    cursor: pointer;
-    width: 128px;
-    height: 40px;
-    border-radius: 100px;
-    color: #FFFFFF;
-    font-family: Lexend;
-    font-size: 14px;
-    font-weight: 500;
-    line-height: 21px;
-    letter-spacing: 0px;
-
-    
-  `;
-
+`;
 
 export default function Catalog() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(
+          "https://api.themoviedb.org/3/movie/popular?api_key=38afe4ec768518a2228d42cf16598485&language=en-US&page=1"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch movies");
+        }
+        const data = await response.json();
+        setMovies(data.results);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    };
+    fetchMovies();
+  }, []);
+
   return (
     <Wrapper>
-        <CartWrapper>
-          <CartContainer>
-            <ArrowLeft>
-              <ImgLeft src={left} alt="left" />
-            </ArrowLeft> 
-            <CartImg src={img1} alt="img1" />
-            <TextCategory>FLOWER</TextCategory>
-            <TextCart>2 Oz Deal Watermelon Zkittles + Purple Gushers</TextCart>
-            <Button>Add to Cart</Button>
+      <CartWrapper>
+        {movies.map((movie, index) => (
+          <CartContainer key={index}>
+            <CartImg
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.title}
+            />
+            <TextName>{movie.title}</TextName>
+            <TextData>({movie.release_date.split("-")[0]})</TextData>
+            <Rating>Rating: {movie.vote_average.toFixed(1)}</Rating>
           </CartContainer>
-          <CartContainer>
-            <CartImg src={img2} alt="img2" />
-            <TextCategory>FLOWER</TextCategory>
-            <TextCart>2 Oz Deal Ahi Tuna + Master Tuna</TextCart>
-            <Button>Add to Cart</Button>
-          </CartContainer>
-          <CartContainer>
-            <ArrowRight>
-              <ImgRight src={right} alt="right" />
-            </ArrowRight> 
-            <CartImg src={img3} alt="img3" />
-            <TextCategory>CONCENTRATES</TextCategory>
-            <TextCart>Mix And Match Shatter/Budder 28g (4 X 7G)</TextCart>
-            <Button>Add to Cart</Button>
-          </CartContainer>
-        </CartWrapper>
+        ))}
+      </CartWrapper>
     </Wrapper>
   );
 }
